c68b97d8e4e121e85c988cf986f4bca1ec55bb2b
Jawad Raza <jawadraza8372@gmail.com>
2022-05-08 09:22:47 +0500

    checking

--- App.js
+++ App.js
@@ -2,6 +2,21 @@ import { StyleSheet, Text, View, StatusBar, SafeAreaView } from "react-native";
 
 // main navigation screen Imported
 import MyStackNavig from "./src/AppNavigation/MyStackNavig";
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
+// jawad razaa
 
 export default function App() {
   return (
